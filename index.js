46fe98019c3127b5f38c6d613ed43446cb0ea273
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-07 12:58:06 +0100

    Test Complete

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+{
+    const num1 = 2;
+    const num2 = 31;
+    const multiply = num1 * num2;
+    multiply;
+}
+
+{
+    let random = Math.ceil(Math.random()+1);
+    random;
+}
+
+
+{
+    const num3 = 24;
+    const num4 = 5;
+    let mod = num3 % num4;
+    mod;
+}
+
+{
+    let max = Math.max(1, 20, 3, 4, 5);
+}
